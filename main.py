2799eb613f7644476153e261bd20e7a7ffbe9fb5
Kye Laliberte <klaliber@icloud.com>
2026-02-19 10:58:41 -0700

    Remove unused testing code from the main

--- main.py
+++ main.py
@@ -22,39 +22,12 @@ def main():
         
         tables=["purchases","stores","categorys","inventory","items","customers"]
 
-        dict=getCustumerByID(conn,3)
-        #shows status of dict
-        if dict is not None:
-            logging.info(f"Customer details: {dict}")
-        else:
-            logging.info("Customer not found or an error occurred.")
-        
-        #shows thecustamer status of dict
-        if dict is not None and 'status' in dict:
-            logging.info(f"Customer status: {dict['status']}")
-        
-        dicts=getItemByID(conn,2)
-        if dicts is not None:
-            logging.info(f"Item details: {dicts}")
-            logging.info(f"Item name: {dicts.get('name', 'N/A')}")
-        else:
-            logging.info("Item not found or an error occurred.")
-        
-        dictss=getStoreByID(conn,1)
-        if dictss is not None:
-            logging.info(f"Store details: {dictss}")
-            logging.info(f"Store name: {dictss.get('name', 'N/A')}")
-
+       
 
 
          # Seed data for categorys
     
 
-        #purchaseslist=gt(conn,"purchases")
-        #for pu in purchaseslist:
-        #        logging.info(pu)
-
-        #s I Q
         
     except psycopg2.Error as e:
         logging.exception(f"data error main.py: {e}")
