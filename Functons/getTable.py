cfb417893b3b90a54510c375be758ebcba6676a7
Kye Laliberte <klaliber@icloud.com>
2026-02-23 15:34:44 -0700

    Refactor getTable function to improve input validation and error handling; use context manager for cursor handling.

--- Functons/getTable.py
+++ Functons/getTable.py
@@ -7,21 +7,23 @@ def getTable(conn, table_name):
      Returns:
         list: A list of tuples containing the records from the specified table."""
     try:
-        if table_name is None or not isinstance(table_name, str):
-            logging.error("A valid table name is required.")
-            return []
-        table_name = table_name.strip().lower()
+        
+        table_name = str(table_name).strip().lower()
+
         if table_name not in ['customers', 'items', 'stores', 'purchases', 'categorys','inventory']:
             logging.error("Invalid table name.")
-            return []
+            return False
 
-        cursor = conn.cursor()
-        query = f"SELECT * FROM {table_name};"
-        cursor.execute(query)
-        records = cursor.fetchall()
+        with conn.cursor() as cursor:
+            query = f"SELECT * FROM {table_name};"
+            cursor.execute(query)
+            records = cursor.fetchall()
+        
         return records
+    
     except psycopg2.Error as e:
         logging.exception(f"Error fetching data from {table_name}: {e}")
-        return []
-    finally:
-        cursor.close()
+        return False
+    except (ValueError, TypeError):
+        logging.exception("input must be a string")
+        return False
